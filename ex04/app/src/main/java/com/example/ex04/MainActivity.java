46db8f9fae83a27501e89fa550f49ca35183ef7e
sumyuktha3 <75235818+sumyuktha3@users.noreply.github.com>
2022-05-19 14:47:48 +0530

    Add files via upload

---
+++ ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ex04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mButton= (Button) findViewById(R.id.send_sms);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","9443354213",null));
+                intent.putExtra("sms_body","Welcome to Android Studio");
+                startActivity(intent);
+            }
+        });
+    }
+}
